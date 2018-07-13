dcf4ad46685785f067e012b713aedf82d18d9ce7
ilanarubin <ilanarubin007@gmail.com>
2018-07-13 22:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function lowerCaseStudentNames (list) {
 
 function nameToAttributes(list){
   return list.map(function(driver) {
-    const fName = driver.split(' ')[0];
-    const lName = driver.split(' ')[1];
-    return {firstName: fName, lastName: lName};
+    const firstName = driver.split(' ')[0];
+    const lastName = driver.split(' ')[1];
+    return {firstName: firstName, lastName: lastName};
   });
 }
 function attributesToPhrase(names){
